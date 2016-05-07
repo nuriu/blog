d6491fd38c68925eac238b65de25666905b69698
nuriu <nuriu@users.noreply.github.com>
2016-05-07 23:02:15 +0300

    yorum sistemi eklendi.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all.order('created_at DESC')
