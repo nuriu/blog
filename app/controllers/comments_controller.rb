603e698407b5b226f9c3db6e42dd393e2b97af5d
nuriu <nuriu@users.noreply.github.com>
2016-05-08 16:05:07 +0300

    stil düzenlemeleri yapıldı.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "admin", password: "admin", only: :destroy
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
-  root 'posts#index'
+  root 'pages#index'
 end
