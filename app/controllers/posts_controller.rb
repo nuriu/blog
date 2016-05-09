1cf36864ae16bbb5684bce4e2022c7914a904d6b
nuriu <nuriu@users.noreply.github.com>
2016-05-09 03:10:17 +0300

    düzenlemeler yapıldı.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order('created_at DESC')
+    @user = User.find(1)
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  root  'pages#index'
+  root 'pages#index'
+  get    'about'   => 'pages#about'
+  get    'contact' => 'pages#contact'
   get    'signup'  => 'users#new'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
